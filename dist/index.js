dfe9c0df2d0b6f581cc6899d761bbfa8af81b87c
Admin <admin@Chris-MacBook-Pro.local>
2024-01-13 13:47:14 -0800

    reverted logging

--- dist/index.js
+++ dist/index.js
@@ -14,8 +14,9 @@ const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
 const serverless = require('serverless-http');
 require('dotenv').config();
-const logger = require('./utils/logging');
-logger.info('Starting up...');
+// const logger = require('./utils/logging');
+// logger.info('Starting up...');
+console.log("Starting up...");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
@@ -64,7 +65,8 @@ const convertYoutubeUrlToMp3 = async (inputUrlRef) => {
         params: { id: inputUrlRef },
         headers: rapidApiCreds
     };
-    logger.info('inputUrlRef: ', inputUrlRef);
+    // logger.info('inputUrlRef: ', inputUrlRef);
+    console.log("inputUrlRef: ", inputUrlRef);
     const response = await (0, axios_1.default)(options); //GET request
     const mp3Url = response.data.link;
     if (response.data.link) {
@@ -84,7 +86,8 @@ const convertYoutubeUrlToMp3 = async (inputUrlRef) => {
             });
         }
         catch (err) {
-            logger.error('Error on writing/converting mp3');
+            // logger.error('Error on writing/converting mp3');
+            console.log("error on writing/converting mp3");
         }
     }
 };
@@ -99,7 +102,8 @@ const getTranscriptionDetails = async (params) => {
             const data = await transcribeClient.send(new client_transcribe_1.GetTranscriptionJobCommand(params));
             const status = (_a = data.TranscriptionJob) === null || _a === void 0 ? void 0 : _a.TranscriptionJobStatus;
             if (status === "COMPLETED") {
-                logger.info('Completed!');
+                // logger.info('Completed!');
+                console.log("completed!");
                 const response = await s3Client.send(command);
                 const result = await ((_b = response.Body) === null || _b === void 0 ? void 0 : _b.transformToString());
                 if (result) {
@@ -113,22 +117,26 @@ const getTranscriptionDetails = async (params) => {
                     resolve();
                 }
                 else {
-                    logger.info('There is no result returned from S3');
+                    // logger.info('There is no result returned from S3');
+                    console.log("There is no result returned from S3");
                 }
             }
             else if (status === "FAILED") {
-                logger.info('Transcription Failed: ' + ((_c = data.TranscriptionJob) === null || _c === void 0 ? void 0 : _c.FailureReason));
+                // logger.info('Transcription Failed: ' + data.TranscriptionJob?.FailureReason);
+                console.log("Transcription failed: ", (_c = data.TranscriptionJob) === null || _c === void 0 ? void 0 : _c.FailureReason);
                 reject((_d = data.TranscriptionJob) === null || _d === void 0 ? void 0 : _d.FailureReason);
             }
             else {
-                logger.info('In Progress...');
+                // logger.info('In Progress...');
+                console.log("In Progress...");
                 setTimeout(() => {
                     getTranscriptionDetails(params).then(resolve).catch(reject);
                 }, 2000);
             }
         }
         catch (err) {
-            logger.error('Error on transcription process');
+            // logger.error('Error on transcription process');
+            console.log("Error on transcription process");
         }
     });
 };
@@ -138,12 +146,14 @@ const getTranscriptionDetails = async (params) => {
  */
 app.post('/transcribe', upload.single('file'), async (req, res) => {
     var _a, _b, _c;
-    logger.info('req.body.inputUrlRef: ', req.body.inputUrlRef);
+    // logger.info('req.body.inputUrlRef: ', req.body.inputUrlRef);
+    console.log("req.body.inputUrlRef: ", req.body.inputUrlRef);
     if (!req.file && !req.body.inputUrlRef) {
         return res.status(400).send({ message: 'No data provided' });
     }
     if (req.file && req.file.size > FIVE_MINUTES) {
-        logger.error('File is too large');
+        // logger.error('File is too large');
+        console.log("file is too large");
         return res.status(400).send({ message: AUDIO_TOO_LARGE });
     }
     let mp3Buffer = (_a = req.file) === null || _a === void 0 ? void 0 : _a.buffer;
@@ -153,14 +163,17 @@ app.post('/transcribe', upload.single('file'), async (req, res) => {
             mp3Buffer = await convertYoutubeUrlToMp3(req.body.inputUrlRef);
         }
         catch (err) {
-            logger.error('Error with inputUrlRef: ');
+            // logger.error('Error with inputUrlRef: ');
+            console.log('Error with inputUrlRef: ');
         }
     }
     else {
-        logger.error('inputUrlRef is invalid: ', req.body.inputUrlRef);
+        // logger.error('inputUrlRef is invalid: ', req.body.inputUrlRef);
+        console.log("inputUrlRef is invalid: ", req.body.inputUrlRef);
     }
     if (mp3Buffer && mp3Buffer.length > FIVE_MINUTES) {
-        logger.error('File is too large');
+        // logger.error('File is too large');
+        console.log("error: file is too large");
         return res.status(400).send({ message: AUDIO_TOO_LARGE });
     }
     logger.info('req.body.jobName: ', req.body.jobName);
@@ -182,10 +195,12 @@ app.post('/transcribe', upload.single('file'), async (req, res) => {
         await s3Client.send(command);
     }
     catch (err) {
-        logger.error("Error when uploading to S3: ");
+        // logger.error("Error when uploading to S3: ", err);
+        console.log("error when uploading to s3: ", err);
     }
     setTimeout(async () => {
-        logger.info("Receiving content from S3, uploading to Transcribe");
+        // logger.info("Receiving content from S3, uploading to Transcribe")
+        console.log("Receiving content from S3, uploading to Transcribe");
         try {
             await transcribeClient.send(new client_transcribe_1.StartTranscriptionJobCommand(params));
             await getTranscriptionDetails(params);
@@ -198,11 +213,13 @@ app.post('/transcribe', upload.single('file'), async (req, res) => {
             }
         }
         catch (err) {
-            logger.error("Error at final stage: ");
+            // logger.error("Error at final stage: ");
+            console.log("error at final stage: ", err);
         }
     }, 2500);
 });
 app.listen(3000, '0.0.0.0', () => {
-    logger.info('Server is running');
+    // logger.info('Server is running');
+    console.log("Server is running");
 });
 module.exports.handler = serverless(app);
