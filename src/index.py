b8439e8bef9130afec1e2a9f37b8757f018907ca
Admin <admin@Chris-MacBook-Pro.local>
2024-02-17 15:05:10 -0800

    Modified .gitignore

--- src/index.py
+++ src/index.py
@@ -5,15 +5,7 @@ import time
 import json
 from pytube import YouTube
 import os
-# from .utils.constants import *
-
-
-BUCKET_NAME = "decipher-audio-files"
-S3_CLIENT = boto3.client('s3')
-TRANSCRIBE_CLIENT = boto3.client('transcribe', region_name='us-west-2')
-FILE_MAX_DURATION_SECONDS = 300
-MAX_RETRIEVE_ATTEMPTS = 50
-YOUTUBE_URL_BASE = "https://www.youtube.com/watch?v="
+from .utils.constants import *
 
 
 # Application object w/ Flask constructor
