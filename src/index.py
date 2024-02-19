781a909c864540810fb1901b1b79691d93324a16
Admin <admin@Chris-MacBook-Pro.local>
2024-02-19 15:20:02 -0800

    Moved /transcribe route to /src/api/routes/transcribe.py

--- src/index.py
+++ src/index.py
@@ -5,17 +5,14 @@ import sys
 import os
 from .api.controllers.transcribe_controller import transcribe
 from .utils.constants import *
+from .api.routes.transcribe import transcribe_blueprint
 
 # Application object w/ Flask constructor
 app = Flask(__name__)
 CORS(app, resources={r"/transcribe": {"origins": "http://localhost:3000"}})
 
-# root/endpoint: /transcribe
-# @app.route decorator - called whenever a request hits the server with this path
-# Request Content-Type: multipart/formData
-@app.route('/transcribe', methods=['POST'])
-def wrapper(): 
-    return transcribe()
+# For joel: endpoint is now /api/transcribe instead of /transcribe. Pull changes from front-end
+app.register_blueprint(transcribe_blueprint, url_prefix='/api')
 
 # Run the Flask server
 if __name__ == "__main__":
